b3f0e738e2af95e07b4dbd4e6ca19444222b3968
ARTIBHARGAVA21 <bhargavarti7@gmail.com>
2025-11-30 20:09:08 +0530

    Pushing the updated flow of code

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://resort-project-j023.onrender.com";
+const API_BASE_URL = "https://resort-project-j023.onrender.com/api";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
